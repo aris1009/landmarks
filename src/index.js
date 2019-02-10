8a1533b87dbf85bf8703d2a5c6eff98c1fdad51e
Aris Konstantoulas <arist.kon@gmail.com>
2019-02-10 03:10:15 +0200

    0.0.1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import {landmarks} from './landmarks'
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(
+  < div >
+    <Card img={landmarks[0].img} name={landmarks[0].name} location={landmarks[0].location}/ >
+      <Card img={landmarks[1].img} name={landmarks[1].name} location={landmarks[1].location}/ >
+        <Card img={landmarks[2].img} name={landmarks[2].name} location={landmarks[2].location}/ >
+        < /div>
+
+          , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: http://bit.ly/CRA - PWA serviceWorker.unregister();
