c3edf46d4438a79e0a024a201025cb028469e080
Aris Konstantoulas <arist.kon@gmail.com>
2019-02-13 22:22:18 +0200

    Scroll feature added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Searchbox from './Searchbox';
+import Scroll from './Scroll'
 import Cardlist from './Cardlist';
-import Searchbox from './Searchbox'
-import { landmarks } from './landmarks';
+import {landmarks} from './landmarks';
 
 export default class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			landmarks: landmarks,
-			searchfield: ''
-		}
-	}
+  constructor() {
+    super();
+    this.state = {
+      landmarks: landmarks,
+      searchfield: ''
+    }
+  }
 
-	onUserIsSearching = e => {
-		this.setState( { searchfield: e.target.value } );
-	}
+  onUserIsSearching = e => {
+    this.setState({searchfield: e.target.value});
+  }
 
-	render() {
-		const filteredLandmarks = this.state.landmarks.filter( landmark => {
-			return landmark.name.toLowerCase().includes( this.state.searchfield.toLowerCase() )
-		} );
-		return ( <div style={{
-				maxWidth: "1350px",
-				margin: "auto"
-		}}>
-			<h1 className="tc">World Landmarks</h1>
-			<Searchbox searching={this.onUserIsSearching}/>
-			<Cardlist landmarks={filteredLandmarks}/>
-		</div> )
-	}
+  render() {
+    const filteredLandmarks = this.state.landmarks.filter(landmark => {
+      return landmark.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    });
+    return (<div>
+      <h1 className="tc pa2 pt2 ma0 f1 lh-title">World Landmarks</h1>
+      <Searchbox searching={this.onUserIsSearching}/>
+			<Scroll>
+				<Cardlist landmarks={filteredLandmarks}/>
+			</Scroll>
+    </div>)
+  }
 }
