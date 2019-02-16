843f41ac97eb11d97def9e8a4a8531e0688cc74a
Aris Konstantoulas <arist.kon@gmail.com>
2019-02-16 22:42:34 +0200

    Redux, yeah!

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import LandmarkImage from './LandmarkImage';
 
 class Card extends Component {
-	render() {
-		const { img, name, location } = this.props;
-		return ( <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-			<LandmarkImage img={img}/>
-			<div>
-				<span>{name}</span><br/>
-				<span>{location}</span>
-			</div>
-		</div> )
-	}
+  render() {
+    const {img, name, location} = this.props;
+    return (<div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+      <LandmarkImage img={img}/>
+      <div>
+        <span>{name}</span><br/>
+        <span>{location}</span>
+      </div>
+    </div>)
+  }
 }
 
 export default Card;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,30 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import Loader from '../components/Loader';
 import Searchbox from '../components/Searchbox';
 import Scroll from '../components/Scroll'
 import Cardlist from '../components/Cardlist';
 
-import ErrorBoundary from '../components/ErrorBoundary'
+import ErrorBoundary from '../components/ErrorBoundary';
 
-export default class App extends Component {
+import {setSearchfield} from '../actions';
+
+const mapStateToProps = (state) => {
+  return {searchfield: state.searchfield}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onUserIsSearching: e => dispatch(setSearchfield(e.target.value))
+  }
+}
+
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      landmarks: [],
-      searchfield: ''
+      landmarks: []
     }
-  }
-
-  onUserIsSearching = e => {
-    this.setState({searchfield: e.target.value});
   }
 
   componentDidMount() {
@@ -32,7 +40,9 @@ export default class App extends Component {
   }
 
   render() {
-    const {landmarks, searchfield} = this.state;
+    const {landmarks} = this.state;
+    const {searchfield, onUserIsSearching} = this.props;
+    console.log(onUserIsSearching);
     const filteredLandmarks = landmarks.filter(landmark => {
       return (landmark.name.toLowerCase().includes(searchfield.toLowerCase()) ||
               landmark.location.toLowerCase().includes(searchfield.toLowerCase()));
@@ -46,7 +56,7 @@ export default class App extends Component {
     } else {
       return (<div>
         <h1 className="tc pa2 pt2 ma0 f1 lh-title">World Landmarks</h1>
-        <Searchbox searching={this.onUserIsSearching}/>
+        <Searchbox searching={onUserIsSearching}/>
         <Scroll>
           <ErrorBoundary>
             <Cardlist landmarks={filteredLandmarks}/>
@@ -56,3 +66,5 @@ export default class App extends Component {
     }
   }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
